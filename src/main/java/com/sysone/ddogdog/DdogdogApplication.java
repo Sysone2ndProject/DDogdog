df4c0cde32b4803ad953bb08218063f6f21ea39b
JUNI <junehee107@naver.com>
2024-09-06 14:59:07 +0900

    Chore : 프로젝트 초기 환경 구성

---
+++ src/main/java/com/sysone/ddogdog/DdogdogApplication.java
@@ -0,0 +1,13 @@
+package com.sysone.ddogdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdogdogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdogdogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sysone/ddogdog/DdogdogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysone.ddogdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdogdogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
