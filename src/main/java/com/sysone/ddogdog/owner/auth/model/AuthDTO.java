9845820361204031f1437708b5dd4f4a8dea2921
김수지 <tvfxq0214@gmail.com>
2024-09-11 14:41:48 +0900

    Feat: Auth 매퍼 및 dto 생성

---
+++ src/main/java/com/sysone/ddogdog/owner/auth/model/AuthDTO.java
@@ -0,0 +1,17 @@
+package com.sysone.ddogdog.owner.auth.model;
+
+import com.sysone.ddogdog.common.config.Role;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class AuthDTO {
+
+    private String id;
+    private String password;
+    private Integer accountNumber;
+    private String bank;
+    private String ownerName;
+    private Role role;
+}
---
+++ src/main/java/com/sysone/ddogdog/owner/auth/repository/AuthMapper.java
@@ -0,0 +1,14 @@
+package com.sysone.ddogdog.owner.auth.repository;
+
+import com.sysone.ddogdog.owner.auth.model.AuthDTO;
+import java.util.Optional;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AuthMapper {
+
+    void signUp(AuthDTO authDTO);
+
+    //아이디 중복 확인
+    Optional<AuthDTO> findById(String id);
+}
