5c6f73f0e7d7609a70d7a3bd58576c32a53e1ff7
김수지 <tvfxq0214@gmail.com>
2024-09-11 14:40:50 +0900

    Feat: 회원가입 컨트롤러 및 테스트 페이지 작성

--- src/main/java/com/sysone/ddogdog/owner/auth/controller/AuthController.java
+++ src/main/java/com/sysone/ddogdog/owner/auth/controller/AuthController.java
@@ -1,5 +1,36 @@
 package com.sysone.ddogdog.owner.auth.controller;
 
+import com.sysone.ddogdog.owner.auth.model.AuthDTO;
+import com.sysone.ddogdog.owner.auth.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/v1/owners")
 public class AuthController {
 
+    @Autowired
+    AuthService authService;
+
+    @GetMapping("/signup")
+    public String signUpForm() {
+        return "owner/signUp";
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Boolean> findId(@PathVariable String id) {
+        return ResponseEntity.ok(authService.findById(id));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signUp(@ModelAttribute AuthDTO authDTO) {
+        authService.signUpOwner(authDTO);
+        return ResponseEntity.ok().build();
+    }
 }
