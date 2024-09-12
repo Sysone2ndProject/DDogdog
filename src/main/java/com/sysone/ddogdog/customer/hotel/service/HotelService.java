20600218d66c0c8d6cf057568781bd38f9da25e0
taeyeon <garong0721@gmail.com>
2024-09-12 21:02:39 +0900

    Feat: 베스트 호텔 조회 서비스, 컨트롤러 구현

---
+++ src/main/java/com/sysone/ddogdog/customer/hotel/service/HotelService.java
@@ -0,0 +1,27 @@
+package com.sysone.ddogdog.customer.hotel.service;
+
+import com.sysone.ddogdog.customer.hotel.mapper.HotelMapper;
+import com.sysone.ddogdog.customer.hotel.model.Hotel;
+import com.sysone.ddogdog.customer.hotel.model.HotelVO;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class HotelService {
+
+    private final HotelMapper hotelMapper;
+
+    public List<HotelVO> getBestHotels() {
+        List<HotelVO> bestHotels = hotelMapper.getBestHotels().stream()
+            .map(hotel -> HotelVO.fromHotelDTO(hotel))
+            .collect(Collectors.toList());
+        return bestHotels;
+    }
+
+    public List<Hotel> getBestHotelsById(Integer customerId) {
+        return hotelMapper.getBestLocalHotels(customerId);
+    }
+}
