1bb3f53efc655c5d2e15f0ee6a46b5488c2d19ac
taeyeon <garong0721@gmail.com>
2024-09-12 21:04:08 +0900

    Feat: 베스트 호텔 조회 Mapper 및 쿼리문 작성

---
+++ src/main/java/com/sysone/ddogdog/customer/hotel/mapper/HotelMapper.java
@@ -0,0 +1,13 @@
+package com.sysone.ddogdog.customer.hotel.mapper;
+
+import com.sysone.ddogdog.customer.hotel.model.Hotel;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface HotelMapper {
+
+    List<Hotel> getBestHotels();
+
+    List<Hotel> getBestLocalHotels(Integer addressId);
+}
