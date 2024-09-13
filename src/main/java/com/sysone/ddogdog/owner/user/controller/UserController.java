ea560ef15e4101824cfc4aa4689306352eec9617
김수지 <tvfxq0214@gmail.com>
2024-09-13 16:55:21 +0900

    Feat:권한 확인용 유저 페이지 및 api 생성

---
+++ src/main/java/com/sysone/ddogdog/owner/user/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.sysone.ddogdog.owner.user.controller;
+
+import com.sysone.ddogdog.owner.auth.model.OwnerDetails;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/v1/owners/user")
+public class UserController {
+
+    @GetMapping()
+    public String userForm(@AuthenticationPrincipal OwnerDetails user, HttpServletRequest httpServletRequest) {
+        return "owner/mypage";
+    }
+}
