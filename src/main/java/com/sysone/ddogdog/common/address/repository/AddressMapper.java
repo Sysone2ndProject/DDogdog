04c77402ab898c45e59b104cca1fabbc16f2cbc7
김수지 <tvfxq0214@gmail.com>
2024-09-18 22:25:50 +0900

    Feat: ADDRESS DTO 및 mapper생성

---
+++ src/main/java/com/sysone/ddogdog/common/address/repository/AddressMapper.java
@@ -0,0 +1,11 @@
+package com.sysone.ddogdog.common.address.repository;
+
+import com.sysone.ddogdog.common.address.model.AddressDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AddressMapper {
+
+
+    void saveAddress(AddressDTO addressDTO);
+}
