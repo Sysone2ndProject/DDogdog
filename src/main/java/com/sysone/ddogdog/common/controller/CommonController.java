9fb41cccae6b3399f903859263c87883665aceac
taeyeon <garong0721@gmail.com>
2024-09-19 01:31:25 +0900

    Refactor: 주소 기반 호텔 추천 수정

--- src/main/java/com/sysone/ddogdog/common/controller/CommonController.java
+++ src/main/java/com/sysone/ddogdog/common/controller/CommonController.java
@@ -1,10 +1,11 @@
 package com.sysone.ddogdog.common.controller;
 
-import com.sysone.ddogdog.customer.hotel.model.Hotel;
+import com.sysone.ddogdog.common.config.oauth.PrincipalDetails;
 import com.sysone.ddogdog.customer.hotel.model.HotelVO;
 import com.sysone.ddogdog.customer.hotel.service.HotelService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,14 +19,19 @@ public class CommonController {
     private final HotelService hotelService;
 
     @GetMapping
-    public String landingMain() {return "common/index";}
+    public String landingMain() {
+        return "common/index";
+    }
 
     @GetMapping("/customers")
-    public String listHotels(Model model) {
+    public String listHotels(@AuthenticationPrincipal PrincipalDetails principalDetails,
+        Model model) {
         List<HotelVO> hotels = hotelService.getBestHotels();
         model.addAttribute("hotels", hotels);
 
-        List<Hotel> localHotels = hotelService.getBestHotelsById(1);
+        Long id = Long.parseLong(principalDetails.getUsername());
+
+        List<HotelVO> localHotels = hotelService.getBestHotelsById(id);
         model.addAttribute("localHotels", localHotels);
 
         return "customer/index";
