cfc840b9e2f78d5f53ec1738ebe6e4e0258c4c21
taeyeon <garong0721@gmail.com>
2024-09-20 17:44:20 +0900

    Refactor: 생성자 어노테이션으로 변경

--- src/main/java/com/sysone/ddogdog/customer/hotel/model/ResponseHotelDetailsDTO.java
+++ src/main/java/com/sysone/ddogdog/customer/hotel/model/ResponseHotelDetailsDTO.java
@@ -2,17 +2,14 @@ package com.sysone.ddogdog.customer.hotel.model;
 
 import com.sysone.ddogdog.customer.room.model.ResponseRoomDTO;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseHotelDetailsDTO {
 
     private ResponseHotelDTO hotel;
     private List<ResponseRoomDTO> rooms;
 
-    public ResponseHotelDetailsDTO() {
-    }
-
-    public ResponseHotelDetailsDTO(ResponseHotelDTO hotel, List<ResponseRoomDTO> rooms) {
-        this.hotel = hotel;
-        this.rooms = rooms;
-    }
 }
