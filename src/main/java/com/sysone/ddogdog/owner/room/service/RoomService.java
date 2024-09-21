5f9703f4c45ba2920fbcac1217dc3ca8a02ef728
김수지 <tvfxq0214@gmail.com>
2024-09-22 02:48:22 +0900

    Feat: 객실등급 detail 값 생성 및 등록가능 객실 리스트 반환 서비스 생성

--- src/main/java/com/sysone/ddogdog/owner/room/service/RoomService.java
+++ src/main/java/com/sysone/ddogdog/owner/room/service/RoomService.java
@@ -8,6 +8,7 @@ import com.sysone.ddogdog.owner.room.model.RoomDTO;
 import com.sysone.ddogdog.owner.room.model.RoomGrade;
 import com.sysone.ddogdog.owner.room.model.ResponseRoomDTO;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -54,5 +55,17 @@ public class RoomService {
             RoomDTO roomDTO = RoomDTO.updateRoomWithoutImg(requestRoomDTO);
             ownerRoomMapper.updateRoomWithoutImg(roomDTO);
         }
+    }
+
+    public List<RoomGrade> getHotelExistGrade(Integer hotelId){
+        //전체 등급 리스트
+        List<RoomGrade> allGrade = List.of(RoomGrade.values());
+        //호텔 보유 등급 리스트
+        List<RoomGrade> existGrades = ownerRoomMapper.getHotelExistGrade(hotelId);
+
+        // 호텔 등록된 객실 존재 시 해당 등급 제외 후 반환, 없을 시 전체 반환
+        return allGrade.stream()
+            .filter(grade -> !existGrades.contains(grade))
+            .collect(Collectors.toList());
     }
 }
