c00a0a154cd810b54e862027efff48a4321501e2
김수지 <tvfxq0214@gmail.com>
2024-09-21 18:26:45 +0900

    Feat: 객실 정보 매퍼 생성

--- src/main/java/com/sysone/ddogdog/owner/room/mapper/OwnerRoomMapper.java
+++ src/main/java/com/sysone/ddogdog/owner/room/mapper/OwnerRoomMapper.java
@@ -2,12 +2,16 @@ package com.sysone.ddogdog.owner.room.mapper;
 
 import com.sysone.ddogdog.owner.room.model.ResponseRoomDTO;
 import com.sysone.ddogdog.owner.room.model.RoomDTO;
+import com.sysone.ddogdog.owner.room.model.RoomGrade;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface OwnerRoomMapper {
+
     void saveRoom(RoomDTO roomDTO);
 
     List<ResponseRoomDTO> getRoomsByHotelId(Integer hotelId);
+
+    ResponseRoomDTO getHotelByIDAndGrade(Integer hotelId, RoomGrade grade);
 }
