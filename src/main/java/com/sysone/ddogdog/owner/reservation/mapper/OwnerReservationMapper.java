4778515bb7db3ac9af8d3aff1225b28a1ead3487
김수지 <tvfxq0214@gmail.com>
2024-09-24 10:00:06 +0900

    Feat: 예약 조회 매퍼 생성

---
+++ src/main/java/com/sysone/ddogdog/owner/reservation/mapper/OwnerReservationMapper.java
@@ -0,0 +1,17 @@
+package com.sysone.ddogdog.owner.reservation.mapper;
+
+import com.sysone.ddogdog.owner.reservation.model.ReservationDTO;
+import com.sysone.ddogdog.owner.reservation.model.ResponseReservationDayDTO;
+import com.sysone.ddogdog.owner.reservation.model.ResponseReservationMonthDTO;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface OwnerReservationMapper {
+    List<ResponseReservationMonthDTO> getMonthReservation( @Param("startDate") String startDate, @Param("endDate") String endDate,@Param("hotelId") Integer hotelId);
+
+    List<ResponseReservationDayDTO> getDayReservation(@Param("hotelId") Integer hotelId, @Param("reservationDate") String reservationDate);
+}
