6d0e4c395743fc315d933c43299a5d54bba8be7b
김수지 <tvfxq0214@gmail.com>
2024-09-24 09:59:25 +0900

    Feat: 월별 일별 예약 목록 조회 컨트롤러 및 서비스 생성

---
+++ src/main/java/com/sysone/ddogdog/owner/reservation/controller/ReservationViewController.java
@@ -0,0 +1,43 @@
+package com.sysone.ddogdog.owner.reservation.controller;
+
+import com.sysone.ddogdog.owner.reservation.model.ResponseReservationDayDTO;
+import com.sysone.ddogdog.owner.reservation.model.ResponseReservationMonthDTO;
+import com.sysone.ddogdog.owner.reservation.service.OwnerReservationService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Slf4j
+@Controller("OwnerReservationController")
+@RequiredArgsConstructor
+@RequestMapping("/v1/owners/reservations")
+public class ReservationViewController {
+
+    private final OwnerReservationService ownerReservationService;
+
+    @GetMapping
+    public String getReservations(@RequestParam("hotelId") Integer hotelId,
+        @RequestParam("year") Integer year,
+        @RequestParam("month") Integer month, Model model) {
+        List<ResponseReservationMonthDTO> reservations = ownerReservationService.getReservation(hotelId, year, month);
+        model.addAttribute("hotelId", hotelId);
+        model.addAttribute("reservations", reservations);
+        return "owner/monthReservation";
+    }
+
+    @GetMapping("/day")
+    public String getReservationsDay(@RequestParam("hotelId") Integer hotelId,
+        @RequestParam("year") Integer year,
+        @RequestParam("month") Integer month,
+        @RequestParam(value = "day") Integer day, Model model) {
+
+        List<ResponseReservationDayDTO> reservations = ownerReservationService.getReservationDay(hotelId, year, month, day);
+        model.addAttribute("reservations", reservations);
+        return "owner/dayReservation";
+    }
+}
