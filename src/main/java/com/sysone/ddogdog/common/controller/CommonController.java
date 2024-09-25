30f2be20bafd938bf479d9151f4f6bf6bbb749b5
JUNI <junehee107@naver.com>
2024-09-26 02:51:00 +0900

    Chore : 실수로 삭제한 공통 Controller 복구

---
+++ src/main/java/com/sysone/ddogdog/common/controller/CommonController.java
@@ -0,0 +1,47 @@
+package com.sysone.ddogdog.common.controller;
+
+import com.sysone.ddogdog.common.config.oauth.PrincipalDetails;
+import com.sysone.ddogdog.customer.hotel.model.ResponseHotelDTO;
+import com.sysone.ddogdog.customer.hotel.service.HotelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/v1")
+public class CommonController {
+    private final HotelService hotelService;
+    @Value("${kakao.map.api-key}")
+    private String kakaoJsId;
+
+    @GetMapping
+    public String landingMain() {
+        return "common/index";
+    }
+
+    @GetMapping("/customers")
+    public String listHotels(@AuthenticationPrincipal PrincipalDetails principalDetails,
+                             Model model) {
+        List<ResponseHotelDTO> hotels = hotelService.getBestHotels();
+        model.addAttribute("hotels", hotels);
+        if (principalDetails != null) {
+            Long id = Long.parseLong(principalDetails.getUsername());
+            List<ResponseHotelDTO> localHotels = hotelService.getBestHotelsById(id);
+            model.addAttribute("localHotels", localHotels);
+            model.addAttribute("kakaoId", kakaoJsId);
+        }
+        return "customer/index";
+    }
+
+    @GetMapping("/owners")
+    public String ownerMain() {
+        return "owner/index";
+    }
+}
