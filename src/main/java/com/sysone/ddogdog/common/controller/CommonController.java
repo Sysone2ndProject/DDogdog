a18be9fb6d0db48c9d3049b8a1801a46701a04a8
JUNI <junehee107@naver.com>
2024-09-26 14:21:28 +0900

    Refactor : 카카오 Appkey 서비스단에서 호출하는것으로 변경

--- src/main/java/com/sysone/ddogdog/common/controller/CommonController.java
+++ src/main/java/com/sysone/ddogdog/common/controller/CommonController.java
@@ -1,25 +1,24 @@
 package com.sysone.ddogdog.common.controller;
 
 import com.sysone.ddogdog.common.config.oauth.PrincipalDetails;
+import com.sysone.ddogdog.customer.auth.service.KakaoService;
 import com.sysone.ddogdog.customer.hotel.model.ResponseHotelDTO;
 import com.sysone.ddogdog.customer.hotel.service.HotelService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/v1")
 public class CommonController {
+
     private final HotelService hotelService;
-    @Value("${kakao.map.api-key}")
-    private String kakaoJsId;
+    private final KakaoService kakaoService;
 
     @GetMapping
     public String landingMain() {
@@ -27,13 +26,14 @@ public class CommonController {
     }
 
     @GetMapping("/customers")
-    public String listHotels(@AuthenticationPrincipal PrincipalDetails principalDetails,
-                             Model model) {
+    public String listHotels(@AuthenticationPrincipal PrincipalDetails user,
+        Model model) {
         List<ResponseHotelDTO> hotels = hotelService.getBestHotels();
         model.addAttribute("hotels", hotels);
-        if (principalDetails != null) {
-            Long id = Long.parseLong(principalDetails.getUsername());
+        if (user != null) {
+            Long id = Long.parseLong(user.getUsername());
             List<ResponseHotelDTO> localHotels = hotelService.getBestHotelsById(id);
+            String kakaoJsId = kakaoService.getKakaoKey();
             model.addAttribute("localHotels", localHotels);
             model.addAttribute("kakaoId", kakaoJsId);
         }
--- src/main/java/com/sysone/ddogdog/customer/auth/service/KakaoService.java
+++ src/main/java/com/sysone/ddogdog/customer/auth/service/KakaoService.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 import com.sysone.ddogdog.customer.auth.model.RequestAddressDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +20,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class KakaoService {
 
     private final KakaoMapper kakaoMapper;
+
+    @Value("${kakao.map.api-key}")
+    private String kakaoJsId;
 
     public Optional<CustomerDTO> findUserById(Long id) {
         return kakaoMapper.findUserById(id);
@@ -33,5 +37,9 @@ public class KakaoService {
     public void updateAddress(RequestAddressDTO requestAddressDTO) {
         System.out.println("진입테스트");
         kakaoMapper.updateAddress(requestAddressDTO);
+    }
+
+    public String getKakaoKey(){
+        return kakaoJsId;
     }
 }
