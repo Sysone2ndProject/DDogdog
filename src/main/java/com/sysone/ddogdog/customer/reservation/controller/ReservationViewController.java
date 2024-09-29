efcb6970be75d6eab8b2d1f06a3a9fe6ee75d63c
JUNI <junehee107@naver.com>
2024-09-29 13:39:55 +0900

    Refactor : 예약 내용 조회 시 페이지네이션 구현

--- src/main/java/com/sysone/ddogdog/customer/reservation/controller/ReservationViewController.java
+++ src/main/java/com/sysone/ddogdog/customer/reservation/controller/ReservationViewController.java
@@ -4,11 +4,13 @@ import com.sysone.ddogdog.common.config.oauth.PrincipalDetails;
 import com.sysone.ddogdog.customer.reservation.model.ResponseReservationDTO;
 import com.sysone.ddogdog.customer.reservation.service.ReservationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,9 +21,12 @@ public class ReservationViewController {
 
     private final ReservationService reservationService;
     @GetMapping
-    public String findReservation(@AuthenticationPrincipal PrincipalDetails user, Model model){
-        List<ResponseReservationDTO> reservationInfos = reservationService.findReservationsByCustomerId(user.getUsername());
+    public String findReservation(@AuthenticationPrincipal PrincipalDetails user, @RequestParam(value = "page", defaultValue = "1") int page,
+                                  @RequestParam(value = "size", defaultValue = "5") int size, Model model){
+        Page<ResponseReservationDTO> reservationInfos = reservationService.findReservationsByCustomerId(user.getUsername(),page,size);
         model.addAttribute("ReservationInfos",reservationInfos);
+        model.addAttribute("currentPage", page);  // 현재 페이지 번호
+        model.addAttribute("totalPages", reservationInfos.getTotalPages());  // 전체 페이지 수
         return "customer/reservationInfo";
     }
 }
