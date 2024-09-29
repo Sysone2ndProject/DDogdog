c830e4f8b1e642ee2c2dc837620dff18d153de71
김수지 <tvfxq0214@gmail.com>
2024-09-29 22:59:28 +0900

    Style: 알럿 아이콘 수정

--- src/main/resources/static/js/owners/header.js
+++ src/main/resources/static/js/owners/header.js
@@ -1,7 +1,7 @@
 const needLogin = () => {
   Swal.fire({
     title: '로그인 후 이용해주세요',
-    icon: 'success',
+    icon: 'error',
   }).then((result) => {
     if (result.isConfirmed) {
       window.location.href = '/v1/owners/login';
